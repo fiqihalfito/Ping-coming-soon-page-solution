0b94df18114871a8701b6666d94e8350f4509c2f
fiqihalfito <fiqihalfito.dev@gmail.com>
2022-12-12 11:28:09 +0700

    added transition

--- components/email-input.jsx
+++ components/email-input.jsx
@@ -4,7 +4,11 @@ const EmailInput = (props) => (
     <form className="grid grid-cols-1 md:grid-cols-3 gap-2" onSubmit={props.onSubmit}>
         <input type="text" placeholder="Your email address" className="text-sm border-2 border-custom-secondary-paleBlue focus:border-custom-primary-blue focus:outline-none py-3 px-8 rounded-full col-span-2" id="email" name="email" />
 
-        <button className="text-sm font-semibold text-white bg-custom-primary-blue hover:bg-custom-secondary-paleBlue active:bg-custom-secondary-lightRed hover:text-custom-primary-blue active:text-white py-3 rounded-full" type="submit">
+        <button className="text-sm font-semibold text-white bg-custom-primary-blue
+        hover:bg-custom-secondary-paleBlue active:bg-custom-secondary-lightRed
+         hover:text-custom-primary-blue active:text-white py-3 rounded-full
+          transition ease-in-out duration-300 active:transition-none
+        " type="submit">
             Notify Me
         </button>
     </form>
--- components/social-media.jsx
+++ components/social-media.jsx
@@ -1,14 +1,33 @@
 
-import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa"
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa"
+import Link from "next/link"
 
-const icons = [<FaFacebookF />, <FaTwitter />, <FaInstagram />]
+const socialmedia = [
+    {
+        icon: <FaGithub />,
+        url: "https://www.github.com/fiqihalfito"
+    },
+    {
+        icon: <FaInstagram />,
+        url: "https://www.instagram.com/fiqihalfito"
+    },
+    {
+        icon: <FaLinkedin />,
+        url: "https://www.linkedin.com/in/fiqih-alfito"
+    }
+]
 
 const SocialMedia = () => (
-    <div className="text-custom-primary-blue flex gap-4 justify-center mb-8">
-        {icons.map((icon, index) => (
-            <div key={index} className="p-2 border border-custom-secondary-paleBlue rounded-full">
-                {icon}
-            </div>
+    <div className="flex gap-4 justify-center mb-8">
+        {socialmedia.map((item, index) => (
+            <Link href={item.url} key={index}>
+                <div className="p-2 border border-custom-secondary-paleBlue rounded-full
+                text-custom-primary-blue hover:text-white hover:bg-custom-primary-blue
+                hover:scale-150 active:scale-75
+                transition-all ease-in-out duration-300 active:duration-100">
+                    {item.icon}
+                </div>
+            </Link>
         ))}
     </div>
 )
